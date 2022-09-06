067d56cc77c2743f7ef2d4531eade370abae7f69
Kaymora Roberts <112441473+kaymoraroberts@users.noreply.github.com>
2022-09-06 22:02:19 +0000

    fixed issues

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,28 @@
+print(10*5)
+print(10*5)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+
+print(10/15) 
+ #the result of this numerical expression would be a fraction which would require a float.
+
+
+#part 2
+
+rate = float(input("What is the current exchange rate for the Euro to  Dollar?"))
+print(rate)
+
+amount= float(input("What is the amount of currency to exchange?"))
+
+print(amount)
+
+total= (rate)*(amount)
+
+result = total - 3
+
+print(result)
