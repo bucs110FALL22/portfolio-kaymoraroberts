71bc7dbaeedd4a3f4a3c3ea0a87722da94701450
Kaymora Roberts <112441473+kaymoraroberts@users.noreply.github.com>
2022-09-07 22:22:38 +0000

    9/7

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,43 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+
+
+my_turtle.shape("turtle")
+my_turtle.color("purple")
+
+length = 50
+angle = 90
+
+my_turtle.forward(length)
+my_turtle.left(angle)
+
+my_turtle.forward(length)
+my_turtle.left(angle)
+
+my_turtle.forward(length)
+my_turtle.left(angle)
+
+my_turtle.forward(length)
+my_turtle.left(angle)
+
+my_turtle.color("red")
+
+y_turtle.forward(length)
+my_turtle.left(angle)
+
+my_turtle.forward(length)
+my_turtle.left(angle)
+
+my_turtle.forward(length)
+my_turtle.left(angle)
+
+my_turtle.forward(length)
+my_turtle.left(angle)
+
+
+
+
+
+
---
+++ ch02/exercises/simple.py
@@ -0,0 +1,26 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+
+
+my_turtle.shape("turtle")
+my_turtle.color("purple")
+length = 50
+
+my_turtle.forward(length)
+my_turtle.left(90)
+
+my_turtle.forward(length)
+my_turtle.left(90)
+
+my_turtle.forward(length)
+my_turtle.left(90)
+
+my_turtle.forward(length)
+my_turtle.left(90)
+
+my_turtle.color("red")
+
+
+screen.exitonclick()
