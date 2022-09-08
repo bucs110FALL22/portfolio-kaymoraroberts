715a4dcff549c35374be7f246a73ee237232f70b
Kaymora Roberts <112441473+kaymoraroberts@users.noreply.github.com>
2022-09-08 22:30:27 +0000

    lab due 9/9

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,29 @@
-
+import random
+ 
 #Part A
 weeks = 16
+print(weeks, type(weeks))
+
 classes = 5
+print(classes, type(classes))
+
 tuition = 6000
+print(tuition, type(tuition))
+
+classes_per_week = 3
+print(classes_per_week, type(classes_per_week))
+
+
+
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+cost_per_class = (cost_per_week / classes_per_week)
+
+print('Your cost per class is' cost_per_class)
+
 
 #Part B
+
+new = list(range(1,6))
+print(random.choice(new))
