ccb3a4a92f55558475d8b3855c5f6cd36b147449
Kaymora Roberts <112441473+kaymoraroberts@users.noreply.github.com>
2022-10-14 20:42:49 +0000

    lab 5

---
+++ ch05/lab/main.py
@@ -0,0 +1,80 @@
+import pygame 
+
+
+#PART a
+
+n=0 
+
+while n > 0:
+  if (n%2 == 0) 
+  n = n/2 
+  print n 
+
+  if (n%2 == 1) 
+  n = n*3 + 1 
+  print n 
+
+  n += 1 
+    else 
+
+# PART b
+
+
+upper_limit = 20
+iters = my_dict {}
+
+start = 0 
+count = 0 
+for n in range(2, upper_limit + 1, 1) 
+
+  while n > 0:
+    count =+ 1
+    if (n%2 == 0) 
+    n = n/2 
+    print n 
+    
+
+    if (n%2 == 1) 
+    n = n*3 + 1 
+    print n 
+
+  n += 1 
+    else 
+ iters = start =+ 2 
+
+print my_dict 
+
+
+#PART C 
+
+max_so_far = count 
+
+
+pygame.draw.lines(display, blue, False, 500,500)
+
+display = pygame.display.set_mode()
+
+screen.fill(white)
+font = pygame.font.Font(none, none)
+
+ msg = font.render(graph, true, blue)
+
+display.blit( graph1, (10,10))
+
+print (max_so_far)
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
