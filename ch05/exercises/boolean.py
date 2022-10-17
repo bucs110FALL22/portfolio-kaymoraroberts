a775f89d9792aa48a733033ef031c9685c32a308
Kaymora Roberts <112441473+kaymoraroberts@users.noreply.github.com>
2022-10-17 21:53:05 +0000

    10/17

---
+++ ch05/exercises/boolean.py
@@ -0,0 +1,38 @@
+
+
+score = 0 
+
+def percentage_to_letter(score)
+  if(score >= 90)
+  return "A"
+
+if(score 90 <= 80)
+  return "B"
+
+if(score 80 <= 70)
+  return "C"
+
+if(score 70 <= 60)
+  return "D"
+
+if (score < 60 )
+  return "F"
+
+
+
+
+
+
+
+#PART 2
+def is_passing(score)
+while score >= 70 
+score = True 
+
+input ("What is your score")
+
+  if( score > 60 ) 
+    print " You receive an passing grade"
+
+else 
+print "You have failed."
