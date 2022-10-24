256e64125006b3ff314cdcb3157ba37cd111f634
Kaymora Roberts <112441473+kaymoraroberts@users.noreply.github.com>
2022-10-24 21:56:57 +0000

    10/24

---
+++ ch06/exercises/gui_class.py
@@ -0,0 +1,20 @@
+class Player 
+
+def __init__(self, playerClothing, player, pnum,  ):
+
+  self.bricks = 3
+  self.clothing = playerClothing 
+  self.background = background
+  
+class coin 
+def __init__(self, coinColor, player, pnum,  ):
+self.coin_num = 5 
+self. color = coinColor
+self.sound - coinSound
+
+class mushrooms 
+def __init__(self, mushColor, player, pnum,  ):
+self.mushroom_num = 2 
+self.color= mushColor
+self.
+
