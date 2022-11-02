c3d6512fe9372e1d5308572b04453830f879f03f
Kaymora Roberts <112441473+kaymoraroberts@users.noreply.github.com>
2022-11-02 21:48:13 +0000

    11/2

---
+++ ch07/exercises/shelter.py
@@ -0,0 +1,38 @@
+import time 
+import random 
+import animal
+
+
+class shelter
+  def __init__(self, type)
+  self.id = self(id)
+  self.name = name
+  self.type = type
+  self.arrived = time.strftime("%d/%m/%Y")
+  self.adopted = none
+  
+def adoptiondate(self)
+  self.adopted = time.strftime("%d/%m/%Y")
+
+
+def main():
+
+      Polo = Animal("Polo", "dog")
+      Polo.setAdopted()
+main()
+
+animal_1: shelter("Polo", "dog")
+date_arrived = time.strftime("%d/%m/%Y")
+date_adopted = time.strftime("%d/%m/%Y")
+identification = randon.randint()
+
+animal_2 : shelter("Oscar", "frog")
+date_arrived = time.strftime("%d/%m/%Y")
+date_adopted = time.strftime("%d/%m/%Y")
+identification = randon.randint()
+               
+amimal_3 : shelter("Marco", "cat")
+date_arrived = time.strftime("%d/%m/%Y")
+date_adopted = time.strftime("%d/%m/%Y")
+identification = randon.randint()
+
